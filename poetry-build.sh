9e809076765e325b2992ad3e7ee3fa92cd450b74
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2024-02-21 14:29:18 +0100

    Use a valid Python version for the package
    Entirely drop the approach of running Maven's string-filters and rather
    parse the package version from 'pom.xml' and postprocess it in case it
    is a SNAPSHOT, using the distance to the last release tag.

--- poetry-build.sh
+++ poetry-build.sh
@@ -19,23 +19,44 @@ if [ -n "$STATUS" ]; then
     exit 1
 fi
 
-# clean up old poetry artifacts:
+### clean up old poetry artifacts:
 rm -rf dist/
-# clean up old maven artifacts:
-rm -rf target/
-# clean up potential leftovers from previous runs of this script:
+### clean up potential leftovers from previous runs of this script:
 rm -rf sjlogging/
 
-# call maven to perform the "string-filtering" which will substitute the version
-# placeholder in __init__.py by the (maven-derived) version string:
-mvn process-resources
+# move the source tree to the project root, so poetry will pick it up
+# automatically from there (without explicit configuration):
+mv src/main/resources/sjlogging .
 
-# move the string-filtered source tree to the project root, so poetry will pick
-# it up automatically from there (without explicit configuration):
-mv target/classes/sjlogging/ .
+### parse the version from 'pom.xml':
+PACKAGE_VERSION=$(xmlstarlet sel --template -m _:project -v _:version pom.xml)
 
-# let poetry build the wheel (and tar.gz)
+### make sure to have a valid Python package version:
+case $PACKAGE_VERSION in
+*-SNAPSHOT*)
+    echo "POM version [$PACKAGE_VERSION] is a snapshot!"
+    PACKAGE_VERSION=${PACKAGE_VERSION/-SNAPSHOT/}
+    ### calculate the distance to the last release tag:
+    LAST_TAG=$(git tag --list 'jython-scijava-logging-*' | sort | tail -n1)
+    # echo "Last git tag: '$LAST_TAG'"
+    COMMITS_SINCE=$(git rev-list "${LAST_TAG}..HEAD" | wc -l)
+    # echo "Nr of commits since last tag: $COMMITS_SINCE"
+    HEAD_ID=$(git rev-parse --short HEAD)
+    # echo "HEAD commit hash: $HEAD_ID"
+    PACKAGE_VERSION="${PACKAGE_VERSION}.dev${COMMITS_SINCE}+${HEAD_ID}"
+    ;;
+esac
+
+echo "Using package version: [$PACKAGE_VERSION]"
+
+### put the version into the project file and the package source:
+sed -i "s/\${project.version}/${PACKAGE_VERSION}/" pyproject.toml
+sed -i "s/\${project.version}/${PACKAGE_VERSION}/" sjlogging/__init__.py
+
+### let poetry build wheel and tar.gz:
 poetry build -vv
 
-# remove the string-filtered source tree:
+### clean up the moved source tree and restore the previous state:
 rm -rf sjlogging/
+git restore pyproject.toml
+git restore src/main/resources/sjlogging/
